59917ce842f919944b0e7294fa3e9081ff02863e
VektorVkusoff <n@>
2025-09-22 12:06:04 +0200

    ara

---
+++ src/zdn01.py
@@ -0,0 +1,9 @@
+price=int(input())
+discount=int(input())
+vat=int(input())
+base=price*(1-discount/100)
+vat_amount=base*(vat/100)
+total=base+vat_amount
+print(base,'$')
+print(vat_amount,'$')
+print(total,'$')
