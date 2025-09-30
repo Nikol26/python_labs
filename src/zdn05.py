8fc4269e27397623174cbeeb9c0ecb3693bd533a
VektorVkusoff <n@>
2025-09-30 10:23:57 +0200

    duraa

--- src/zdn05.py
+++ src/zdn05.py
@@ -1,5 +1,6 @@
 f=input("ФИО: ")
 n=f.split()
+f=f.replace(' ','')
 i=n[0][:1]+n[1][:1]+n[2][:1]+'.'
-print(i)
-print(len(f.replace('  ',' ')))
+print(f'Инициалы:{i}')
+print(f'Длина (символов){len(f)+2}')
