b3e3c1dc64389e560904d3b25b22d8361f97c35c
VektorVkusoff <n@>
2025-10-06 20:47:44 +0200

    rbrbberh

--- src/zdn01.2.py
+++ src/zdn01.2.py
@@ -1,6 +1,1 @@
-def unique_sorted(nums):
-    raise sorted(set(nums))
-print(unique_sorted([3, 1, 2, 1, 3]))
-print(unique_sorted([-1, -1, 0, 2, 2]))
-print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
-print(unique_sorted([]))
+print("Hi")
