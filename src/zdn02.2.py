ebb26b1da325dc8ba2f44bffdc0371861fc1be0d
VektorVkusoff <n@>
2025-10-07 00:26:53 +0200

    rvrt

---
+++ src/zdn02.2.py
@@ -0,0 +1,16 @@
+def check_rectangular(mat):
+    if not mat:
+        return True
+    length = len(mat[0])
+    for row in mat:
+        if len(row) != length:
+            return False
+    return True
+def row_sums(mat):
+    if not check_rectangular(mat):
+        raise ValueError("Рваная матрица")
+    return [sum(row) for row in mat]
+print(row_sums([[1, 2, 3], [4, 5, 6]]))
+print(row_sums([[-1, 1], [10, -10]]))
+print(row_sums([[0, 0], [0, 0]]))
+print(row_sums([[1, 2], [3]]))
