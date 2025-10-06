eb964adb5b7aaf93c50d49de26a84ed8dfc4093e
VektorVkusoff <n@>
2025-10-07 00:21:35 +0200

    frfr

--- src/zdn02.py
+++ src/zdn02.py
@@ -1,1 +1,20 @@
-print("fcsds")
+def check_rectangular(mat):
+    if not mat:
+        return True
+    length = len(mat[0])
+    for row in mat:
+        if len(row) != length:
+            return False
+    return True
+def transpose(mat):
+    if not check_rectangular(mat):
+        raise ValueError("Рваная матрица")
+    if not mat:
+        return []
+    return [list(row) for row in zip(*mat)]
+
+print(transpose([[1, 2, 3]]))
+print(transpose([[1], [2], [3]]))
+print(transpose([[1, 2], [3, 4]]))
+print(transpose([]))
+print(transpose([[1, 2], [3]]))
