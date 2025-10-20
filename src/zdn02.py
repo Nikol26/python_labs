88950c5e6a67b0271ac65d855e740f8cec3354d7
nicole <korbut.nikol@gmail.com>
2025-10-21 00:10:40 +0200

    334535

--- src/zdn02.py
+++ src/zdn02.py
@@ -1,8 +1,30 @@
-a=(input("a:"))
-b=(input("b:"))
-a=a.replace(",",'.',1)
-b=b.replace(",",".",1)
-a=float(a)
-b=float(b)
-print(f"sum={(a+b):.2f};avg={((a+b)/2):.223"
-      f"45f}")
+from pathlib import Path
+import csv
+import os
+from typing import Iterable, Sequence
+def read_text(path: str | Path, encoding: str = "utf-8") -> str:
+    try:
+        return Path(path).read_text(encoding=encoding)
+    except FileNotFoundError:
+        return "Такого файла нету"
+    except UnicodeDecodeError:
+        return "Неудалось изменить кодировку"
+def write_csv(rows: list[tuple | list], path: str | Path, header: tuple[str, ...] | None = None) -> None:
+    p = Path(path)
+    with p.open('w', newline="", encoding="utf-8") as file:
+        file_c = csv.writer(file)   
+        if header is None and rows == []:
+            file_c.writerow(('a', 'b'))
+        if header is not None:
+            file_c.writerow(header)
+        if rows != []:
+            const = len(rows[0])
+            for i in rows:
+                if len(i) != const:
+                    return ValueError
+        file_c.writerows(rows)
+def ensure_parent_dir(path: str | Path) -> None:
+    path = os.path.dirname(path)
+    Path(path).mkdir(parents=True, exist_ok=True)
+print(read_text(r"C:\Users\VektorVkusoff\OneDrive\Документы\GitHub\python_labs\data\input.txt"))
+write_csv([("word","count"),("test",3)], r"C:\Users\VektorVkusoff\OneDrive\Документы\GitHub\python_labs\data\check.csv") 
